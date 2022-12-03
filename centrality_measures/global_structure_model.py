c6d387f0242c556fb913d1095f2d60f1451365cb
Nagy-Sándor Máté <sandorm.mate@gmail.com>
2022-12-03 21:25:28 +0100

    finishing global_structure_model centrality

--- centrality_measures/global_structure_model.py
+++ centrality_measures/global_structure_model.py
@@ -1,5 +1,6 @@
 from utils.k_shell import KShell
 import math
+import networkx as nx
 
 
 class GlobalStructureModel:
@@ -13,8 +14,15 @@ class GlobalStructureModel:
 
     def calculate_influences(self):
         for node in self.graph.nodes:
+            global_influence = 0
             influence = math.pow(math.e, (self.graph.nodes[node]["k-shell"] / len(self.graph.nodes)))
 
             self.graph.nodes[node]["self-influence"] = influence
-            print(str(node) + " :" + str(influence))
+
+            for next_node in self.graph.nodes:
+                if next_node != node:
+                    global_influence += self.graph.nodes[next_node]["k-shell"] / \
+                                    (len(nx.shortest_path(self.graph, source=node, target=next_node)) - 1)
+
+            self.centrality_values[node] = influence * global_influence
 
